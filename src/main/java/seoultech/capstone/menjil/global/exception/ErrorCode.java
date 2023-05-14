069aaa2b8bf936bfba646f47e45b6b650745b133
Moojun <mgim32137@gmail.com>
2023-05-14 15:58:55 +0900

    #3 - feat: Add common error code

--- src/main/java/seoultech/capstone/menjil/global/exception/ErrorCode.java
+++ src/main/java/seoultech/capstone/menjil/global/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // Common
-    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C001", "Internal server error"),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C001", " Invalid Input Value"),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C002", " Method not allowed"),
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C003", "Internal server error"),
 
     // User
     NICKNAME_DUPLICATED(HttpStatus.CONFLICT, "U001", "Nickname is already existed"),
