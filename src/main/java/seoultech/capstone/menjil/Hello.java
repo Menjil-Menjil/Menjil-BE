21d47af508b25ff9abf6416c87d1f5a54f60c4b1
Moojun <mgim32137@gmail.com>
2023-05-21 16:27:01 +0900

    feat: Solve CORS issues - nextjs 프록시 설정 없이 서버에서 해결하는 방식으로 - Hello, HelloController는 GET, POST 요청 테스트를 위해 남겨놓음

---
+++ src/main/java/seoultech/capstone/menjil/Hello.java
@@ -0,0 +1,9 @@
+package seoultech.capstone.menjil;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+    public int a;
+    public String b;
+}
--- src/main/java/seoultech/capstone/menjil/HelloController.java
+++ src/main/java/seoultech/capstone/menjil/HelloController.java
@@ -1,6 +1,9 @@
 package seoultech.capstone.menjil;
 
+import lombok.Builder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +11,27 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello";
+        System.out.println("hello 요청 들어옴");
+        return "http://menjil-menjil.com:8081 의 GET 요청이 잘 들어왔습니다.";
+    }
+
+    @PostMapping("/hello2")
+    public Test2 test(@RequestBody Hello hello) {
+        return Test2.builder()
+                .a(hello.a)
+                .b("POST 요청이 잘 들어왔습니다")
+                .build();
+    }
+
+
+    static class Test2 {
+        public int a;
+        public String b;
+
+        @Builder
+        public Test2(int a, String b) {
+            this.a = a;
+            this.b = b;
+        }
     }
 }
