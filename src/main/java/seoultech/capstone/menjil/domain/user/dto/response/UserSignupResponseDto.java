b1f1e61bb508d3226ae1e09dcbbc3b801a8906c2
Moojun <mgim32137@gmail.com>
2023-06-16 21:54:11 +0900

    style: status type을 int로 변경

--- src/main/java/seoultech/capstone/menjil/domain/user/dto/response/UserSignupResponseDto.java
+++ src/main/java/seoultech/capstone/menjil/domain/user/dto/response/UserSignupResponseDto.java
@@ -6,13 +6,13 @@ import seoultech.capstone.menjil.domain.user.domain.User;
 
 @Getter
 public class UserSignupResponseDto {
-    private HttpStatus status;
+    private int status;
     private String email;
     private String name;
     private String message;
 
     public UserSignupResponseDto(User user) {
-        this.status = HttpStatus.CREATED;
+        this.status = 201;  // created
         this.email = user.getEmail();
         this.name = user.getName();
         this.message = "가입 요청이 정상적으로 처리되었습니다";
