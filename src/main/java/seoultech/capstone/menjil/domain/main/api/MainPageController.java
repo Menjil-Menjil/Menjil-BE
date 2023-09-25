9979e3f454ba94f1fb518f9ab4e26b851f055a15
Moojun <mgim32137@gmail.com>
2023-09-25 20:24:04 +0900

    #108 - feat: 채팅방 전체 목록을 불러오는 API 추가 및 채팅 목록을 불러오는 API 제거
    * MainPageControllerTest, MainPageServiceTest code coverage 100% complete

--- src/main/java/seoultech/capstone/menjil/domain/main/api/MainPageController.java
+++ src/main/java/seoultech/capstone/menjil/domain/main/api/MainPageController.java
@@ -12,9 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import seoultech.capstone.menjil.domain.chat.application.RoomService;
 import seoultech.capstone.menjil.domain.chat.dto.response.RoomInfoResponse;
 import seoultech.capstone.menjil.domain.main.application.MainPageService;
-import seoultech.capstone.menjil.domain.main.dto.response.FollowListResponse;
+import seoultech.capstone.menjil.domain.main.dto.response.FollowUserResponse;
 import seoultech.capstone.menjil.domain.main.dto.response.MentorInfoResponse;
 import seoultech.capstone.menjil.global.common.dto.ApiResponse;
 import seoultech.capstone.menjil.global.exception.SuccessCode;
@@ -27,41 +28,51 @@ import java.util.List;
 @RequestMapping(value = "/api/main")
 public class MainPageController {
 
+    private final RoomService roomService;
     private final MainPageService mainPageService;
     private final int pageSize = 3;
 
     @GetMapping("/mentors")
-    public ResponseEntity<ApiResponse<Page<MentorInfoResponse>>> getMentorList(String nickname, @PageableDefault(size = pageSize, sort = {"createdDate", "nickname"},
+    public ResponseEntity<ApiResponse<Page<MentorInfoResponse>>> getMentors(String nickname, @PageableDefault(size = pageSize, sort = {"createdDate", "nickname"},
             direction = Sort.Direction.ASC) Pageable pageable) {
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(SuccessCode.GET_MENTOR_LIST_AVAILABLE, mainPageService.getMentorList(nickname, pageable)));
+                .body(ApiResponse.success(SuccessCode.GET_MENTOR_LIST_AVAILABLE, mainPageService.getMentors(nickname, pageable)));
     }
 
-    @GetMapping("/userinfo")
-    public ResponseEntity<ApiResponse<List<RoomInfoResponse>>> getAllRoomsOfUser(@RequestParam("nickname") String nickname) {
-        List<RoomInfoResponse> roomInfoResponseList = mainPageService.getAllRoomsOfUser(nickname);
+    @GetMapping("/rooms")
+    public ResponseEntity<ApiResponse<List<RoomInfoResponse>>> getAllRoomsOfUser(@RequestParam("nickname") String nickname,
+                                                                                 @RequestParam("type") String type) {
+        List<RoomInfoResponse> result = roomService.getAllRoomsOfUser(nickname, type);
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(SuccessCode.GET_USER_ROOMS_AVAILABLE, roomInfoResponseList));
+        if (userHasNoRooms(result)) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.success(SuccessCode.GET_ROOMS_AND_NOT_EXISTS, result));
+        } else {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.success(SuccessCode.GET_ROOMS_AVAILABLE, result));
+        }
     }
 
     /**
      * 관심 멘토의 목록을 불러온다
      */
     @GetMapping("/following")
-    public ResponseEntity<ApiResponse<List<FollowListResponse>>> getFollowersOfUser(@RequestParam("nickname") String nickname) {
-        List<FollowListResponse> followersOfUser = mainPageService.getFollowersOfUser(nickname);
+    public ResponseEntity<ApiResponse<List<FollowUserResponse>>> getFollowersOfUser(@RequestParam("nickname") String nickname) {
+        List<FollowUserResponse> followersOfUser = mainPageService.getFollowersOfUser(nickname);
         if (isUserHasFollows(followersOfUser)) {
             return ResponseEntity.status(HttpStatus.OK)
-                    .body(ApiResponse.success(SuccessCode.FOLLOWS_NOT_EXISTS, followersOfUser));
+                    .body(ApiResponse.success(SuccessCode.FOLLOWS_EXISTS, followersOfUser));
         }
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(SuccessCode.FOLLOWS_EXISTS, followersOfUser));
+                .body(ApiResponse.success(SuccessCode.FOLLOWS_NOT_EXISTS, followersOfUser));
     }
 
-    public boolean isUserHasFollows(List<FollowListResponse> followersOfUser) {
-        return followersOfUser.isEmpty();
+    private boolean userHasNoRooms(List<RoomInfoResponse> list) {
+        return list.isEmpty();
     }
 
+    private boolean isUserHasFollows(List<FollowUserResponse> followersOfUser) {
+        return !followersOfUser.isEmpty();
+    }
 }
--- src/main/java/seoultech/capstone/menjil/domain/main/application/MainPageService.java
+++ src/main/java/seoultech/capstone/menjil/domain/main/application/MainPageService.java
@@ -11,26 +11,19 @@ import org.springframework.stereotype.Service;
 import seoultech.capstone.menjil.domain.auth.dao.UserRepository;
 import seoultech.capstone.menjil.domain.auth.domain.User;
 import seoultech.capstone.menjil.domain.auth.domain.UserRole;
-import seoultech.capstone.menjil.domain.chat.dao.MessageRepository;
 import seoultech.capstone.menjil.domain.chat.dao.QaListRepository;
-import seoultech.capstone.menjil.domain.chat.dao.RoomRepository;
-import seoultech.capstone.menjil.domain.chat.domain.ChatMessage;
 import seoultech.capstone.menjil.domain.chat.domain.QaList;
-import seoultech.capstone.menjil.domain.chat.domain.Room;
-import seoultech.capstone.menjil.domain.chat.dto.response.RoomInfoResponse;
-import seoultech.capstone.menjil.domain.follow.domain.Follow;
 import seoultech.capstone.menjil.domain.follow.dao.FollowRepository;
-import seoultech.capstone.menjil.domain.main.dto.response.FollowListResponse;
+import seoultech.capstone.menjil.domain.follow.domain.Follow;
+import seoultech.capstone.menjil.domain.main.dto.response.FollowUserResponse;
 import seoultech.capstone.menjil.domain.main.dto.response.MentorInfoResponse;
-import seoultech.capstone.menjil.global.exception.CustomException;
-import seoultech.capstone.menjil.global.exception.ErrorCode;
 import seoultech.capstone.menjil.global.handler.AwsS3Handler;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -40,22 +33,19 @@ public class MainPageService {
     private final AwsS3Handler awsS3Handler;
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
-    private final RoomRepository roomRepository;
-    private final MessageRepository messageRepository;
     private final QaListRepository qaListRepository;
 
-    private final int GET_ROOM_INFO_SIZE = 1;
     private final int AWS_URL_DURATION = 7;
 
     @Value("${cloud.aws.s3.bucket}")
     private String BUCKET_NAME;
 
     /**
-     * 멘토 리스트를 가져오는 메서드
+     * 멘토들을 가져오는 메서드
      * nickname은, 추후 멘토 추천 알고리즘 사용시 필요할 수 있으므로, 우선 받도록 하였으나.
      * 현재 수행하는 기능은 없다.
      */
-    public Page<MentorInfoResponse> getMentorList(String nickname, Pageable pageable) {
+    public Page<MentorInfoResponse> getMentors(String nickname, Pageable pageable) {
         Page<User> page = userRepository.findUsersByRole(UserRole.MENTOR, pageable);
         Page<MentorInfoResponse> mentorInfoResponse = page.map(user -> {
             MentorInfoResponse dto = MentorInfoResponse.fromUserEntity(user);
@@ -71,48 +61,9 @@ public class MainPageService {
         return mentorInfoResponse;
     }
 
-    /**
-     * 사용자의 채팅방 목록을 가져온다.
-     * RoomService의 getAllRooms() 메소드와 유사함
-     */
-    public List<RoomInfoResponse> getAllRoomsOfUser(String nickname) {
-        List<RoomInfoResponse> result = new ArrayList<>();
-
-        User user = userRepository.findUserByNickname(nickname).orElse(null);
-        if (user == null) {
-            throw new CustomException(ErrorCode.NICKNAME_NOT_EXISTED);
-        }
-        UserRole role = user.getRole();
-
-        /* 사용자가 멘티인 경우 */
-        if (role.equals(UserRole.MENTEE)) {
-            // 1. 우선 사용자인 멘티의 닉네임으로 전체 채탕방을 조회한다.
-            List<Room> roomList = roomRepository.findRoomsByMenteeNickname(nickname);
-            if (roomList.isEmpty()) {
-                // 채팅방이 하나도 없는 경우
-                return result;
-            }
-            for (Room room : roomList) {
-                result.add(findMentorRoomsByMentee(room));
-            }
-        }
-        /* 사용자가 멘토인 경우 */
-        else {
-            List<Room> roomList = roomRepository.findRoomsByMentorNickname(nickname);
-            if (roomList.isEmpty()) {
-                // 채팅방이 하나도 없는 경우
-                return result;
-            }
-            for (Room room : roomList) {
-                result.add(findMenteeRoomsByMentor(room));
-            }
-        }
-        result = sortByLastMessagedTimeOfHourDESC(result);
-        return result;
-    }
-
-    public List<FollowListResponse> getFollowersOfUser(String nickname) {
-        List<User> followUsers = followRepository.findFollowsByUserNickname(nickname)
+    public List<FollowUserResponse> getFollowersOfUser(String nickname) {
+        // TODO: 이 부분도 join을 사용하거나 혹은 User & Follow 엔티티 사이에 연관 관계를 설정한다면, query 성능을 조금 더 최적화가 가능할 것으로 보임.
+        List<User> followUsers = followRepository.findFollowsByUserNicknameOrderByCreatedDateAsc(nickname)
                 .stream()
                 .map(Follow::getFollowNickname)
                 .map(userRepository::findUserByNickname)
@@ -120,12 +71,12 @@ public class MainPageService {
                 .map(Optional::get)
                 .collect(Collectors.toList());
 
-        // 팔로우를 가장 최근에 한 사용자를 리스트 인덱스 0에 두도록 변경.
+        // 팔로우를 가장 최근에 한 사용자, 즉 createdDate가 가장 나중인 사용자가 리스트 인덱스 0에 오도록 변경
         Collections.reverse(followUsers);
 
         return followUsers.stream()
                 .map(user -> {
-                    FollowListResponse response = FollowListResponse.fromUserEntity(user);
+                    FollowUserResponse response = FollowUserResponse.fromUserEntity(user);
                     response.setImgUrl(String.valueOf(awsS3Handler.generatePresignedUrl(
                             BUCKET_NAME, response.getImgUrl(), Duration.ofDays(AWS_URL_DURATION))));
                     return response;
@@ -133,87 +84,21 @@ public class MainPageService {
                 .collect(Collectors.toList());
     }
 
-    /**
-     * 사용자가 '멘티'인 경우
-     * '멘토'의 방 정보를 받아온다.
-     */
-    private RoomInfoResponse findMentorRoomsByMentee(Room room) {
-        // Get MENTOR nickname, and Room id
-        String mentorNickname = room.getMentorNickname();
-        String roomId = room.getId();
-
-        // Get MENTOR img url
-        User mentor = userRepository.findUserByNickname(mentorNickname)
-                .orElse(null);
-        assert mentor != null;  // 멘토가 존재하지 않을 수 없다.
-
-        // 주의! 만료 기간은 최대 7일까지 설정 가능하다.
-        String mentorImgUrl = String.valueOf(awsS3Handler.generatePresignedUrl(BUCKET_NAME, mentor.getImgUrl(), Duration.ofDays(AWS_URL_DURATION)));
-
-        // Get Last Message and message time
-        PageRequest pageRequest = PageRequest.of(0, GET_ROOM_INFO_SIZE, Sort.by(
-                Sort.Order.desc("time"),
-                Sort.Order.desc("_id")
-        ));
-        List<ChatMessage> messagePage = messageRepository.findChatMessageByRoomId(roomId, pageRequest);
-        String lastMessage = messagePage.get(0).getMessage();
-        LocalDateTime lastMessageTime = messagePage.get(0).getTime();
-
-        return RoomInfoResponse.of(roomId, mentorNickname,
-                mentorImgUrl, lastMessage, lastMessageTime);
-    }
-
-    /**
-     * 사용자가 '멘토'인 경우
-     * '멘티'의 방 정보를 받아온다.
-     */
-    private RoomInfoResponse findMenteeRoomsByMentor(Room room) {
-        // Get MENTEE nickname, and Room id
-        String menteeNickname = room.getMenteeNickname();
-        String roomId = room.getId();
-
-        // Get MENTEE img url
-        User mentee = userRepository.findUserByNickname(menteeNickname)
-                .orElse(null);
-        assert mentee != null;  // 멘티가 존재하지 않을 수 없다.
-        // 주의! 만료 기간은 최대 7일까지 설정 가능하다.
-        String menteeImgUrl = String.valueOf(awsS3Handler.generatePresignedUrl(BUCKET_NAME, mentee.getImgUrl(), Duration.ofDays(AWS_URL_DURATION)));
-
-        // Get Last Message and message time
-        PageRequest pageRequest = PageRequest.of(0, GET_ROOM_INFO_SIZE, Sort.by(
-                Sort.Order.desc("time"),
-                Sort.Order.desc("_id")
-        ));
-        List<ChatMessage> messagePage = messageRepository.findChatMessageByRoomId(roomId, pageRequest);
-        String lastMessage = messagePage.get(0).getMessage();
-        LocalDateTime lastMessageTime = messagePage.get(0).getTime();
-
-        return RoomInfoResponse.of(roomId, menteeNickname,
-                menteeImgUrl, lastMessage, lastMessageTime);
-    }
-
-    /**
-     * Sort by LastMessageTime, order by DESC
-     * 클라이언트에서 처리를 원활하게 하기 위해서 정렬하였음.
-     */
-    private List<RoomInfoResponse> sortByLastMessagedTimeOfHourDESC(List<RoomInfoResponse> result) {
-        result = result.stream()
-                .sorted(Comparator.comparing(RoomInfoResponse::getLastMessageTime).reversed())
-                .collect(Collectors.toList());
-        return result;
-    }
-
-    private List<String> getLastAnsweredMessages(String mentorNickname) {
-        Pageable pageable = PageRequest.of(0, 2, Sort.by(Sort.Direction.DESC, "question_time", "id")); // Get only the first 2 documents and sort them by 'question_time' and 'id' in descending order
+    protected List<String> getLastAnsweredMessages(String mentorNickname) {
+        int page = 0;
+        int size = 2;
+        // Get only the first 2 documents and sort them by 'question_time' and 'id' in descending order
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "question_time", "id"));
         List<QaList> qaLists = qaListRepository.findAnsweredQuestionsByMentor(mentorNickname, pageable);
 
-        if (qaLists.isEmpty()) {
-            return new ArrayList<>();
-        } else if (qaLists.size() == 1) {
-            return Stream.of(qaLists.get(0))
-                    .map(QaList::getQuestionSummary)
-                    .collect(Collectors.toList());
-        }
-        return List.of(qaLists.get(0).getQuestionSummary(), qaLists.get(1).getQuestionSummary());
+        /*
+         At first, checked if qaLists is empty(), or size == 1, or else.
+         But the stream() and map() method calls will produce an empty list if qaLists is empty,
+         and will otherwise produce a list of question summaries,
+         making the explicit checks for qaLists.isEmpty() and qaLists.size() == 1 unnecessary.
+         */
+        return qaLists.stream()
+                .map(QaList::getQuestionSummary)
+                .collect(Collectors.toList());
     }
 }
