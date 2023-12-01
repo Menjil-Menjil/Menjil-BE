17f3014cb0efaacf2652a4a04c1af64b44978228
Moojun <mgim32137@gmail.com>
2023-12-02 03:38:42 +0900

    chore: Separate JpaAuditingConfig

---
+++ src/main/java/seoultech/capstone/menjil/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package seoultech.capstone.menjil.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
